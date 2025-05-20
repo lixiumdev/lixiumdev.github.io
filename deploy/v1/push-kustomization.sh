45421ff73c9e0829644a63119c1a95c1c273dcd6
Nicu Frumusache <nickphroo@proton.me>
2025-05-20 16:08:25 +0200

    feat: push script for kuztomization scenarios

---
+++ deploy/v1/push-kustomization.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# it is assumed that "yq" is installed by default here!
+
+git config --global user.name "GitHub Actions Bot"
+git config --global user.email "contact@lixium.dev"
+
+DEPLOYMENT_REPO_OWNER=lixiumdev
+DEPLOYMENT_REPO_NAME=deployment
+DEPLOYMENT_REPO_URL=https://$GA_BOT_PAT@github.com/$DEPLOYMENT_REPO_OWNER/$DEPLOYMENT_REPO_NAME.git
+
+git clone $DEPLOYMENT_REPO_URL
+cd $DEPLOYMENT_REPO_NAME
+
+DEPLOYMENT_BRANCH="$CLUSTER_NAME"
+
+git switch -c $DEPLOYMENT_BRANCH --track origin/$DEPLOYMENT_BRANCH
+
+WORKLOAD_FOLDER="./src/_kustomize/$SERVICE_NAME/overlays/$CLUSTER_NAMESPACE/"
+
+if [ ! -d "$WORKLOAD_FOLDER" ]; then
+  echo "Could not find the workload folder for $SERVICE_NAME! (cluster: $CLUSTER_NAME, namespace: $CLUSTER_NAMESPACE)"
+  exit 1
+fi
+
+cd $WORKLOAD_FOLDER
+
+IMAGE="$REGISTRY_ADDRESS/$REGISTRY_NAME/$SERVICE_NAME:$GITHUB_REF_NAME"
+image=$IMAGE yq -i '.spec.template.spec.containers[0].image=env(image)' patch-deployment.yml
+
+git add patch-deployment.yml
+
+git commit -am "PIPELINE :: Image updated for \"$SERVICE_NAME\""
+
+git pull --rebase origin $DEPLOYMENT_BRANCH
+git push origin $DEPLOYMENT_BRANCH
